81c2c2fca3971282296ec1702b75245f7e7b85ff
tkawakita <tommycsth61@gmail.com>
2023-01-22 23:11:45 +0900

    中置演算子の評価の実装

--- src/evaluator/evaluator.test.ts
+++ src/evaluator/evaluator.test.ts
@@ -9,6 +9,17 @@ test("整数の評価", () => {
     { input: "10", expected: 10 },
     { input: "-5", expected: -5 },
     { input: "-10", expected: -10 },
+    { input: "5 + 5 + 5 + 5 - 10", expected: 10 },
+    { input: "2 * 2 * 2 * 2 * 2", expected: 32 },
+    { input: "-50 + 100 + -50", expected: 0 },
+    { input: "5 * 2 + 10", expected: 20 },
+    { input: "5 + 2 * 10", expected: 25 },
+    { input: "20 + 2 * -10", expected: 0 },
+    { input: "50 / 2 * 2 + 10", expected: 60 },
+    { input: "2 * (5 + 10)", expected: 30 },
+    { input: "3 * 3 * 3 + 10", expected: 37 },
+    { input: "3 * (3 * 3) + 10", expected: 37 },
+    { input: "(5 + 10 * 2 + 15 / 3) * 2 + -10", expected: 50 },
   ];
 
   tests.forEach(({ input, expected }) => {
@@ -23,6 +34,23 @@ test("真偽値の評価", () => {
   const tests = [
     { input: "true", expected: true },
     { input: "false", expected: false },
+    { input: "1 < 2", expected: true },
+    { input: "1 > 2", expected: false },
+    { input: "1 < 1", expected: false },
+    { input: "1 > 1", expected: false },
+    { input: "1 == 1", expected: true },
+    { input: "1 != 1", expected: false },
+    { input: "1 == 2", expected: false },
+    { input: "1 != 2", expected: true },
+    { input: "true == true", expected: true },
+    { input: "false == false", expected: true },
+    { input: "true == false", expected: false },
+    { input: "true != false", expected: true },
+    { input: "false == true", expected: false },
+    { input: "(1 < 2) == true", expected: true },
+    { input: "(1 < 2) == false", expected: false },
+    { input: "(1 > 2) == true", expected: false },
+    { input: "(1 > 2) == false", expected: true },
   ];
 
   tests.forEach(({ input, expected }) => {
@@ -51,7 +79,6 @@ test("!演算子の評価", () => {
     testBooleanObject(evaluated, expected);
   });
 });
-
 
 const testEval = (input: string): Object_ | null => {
   const l = new Lexer(input);
--- src/evaluator/evaluator.ts
+++ src/evaluator/evaluator.ts
@@ -6,9 +6,11 @@ import {
   ASTStatement,
   ASTBooleanLiteral,
   ASTPrefixExpression,
+  ASTInfixExpression,
 } from "../ast/ast";
 import {
   Boolean_,
+  BOOLEAN_OBJ,
   Integer,
   INTEGER_OBJ,
   Null,
@@ -37,6 +39,13 @@ export const evaluate = (node: ASTNode | null): Object_ | null => {
     if (right === null) return null;
 
     return evaluatePrefixExpression(node.operator, right);
+  }
+  if (node instanceof ASTInfixExpression) {
+    const left = evaluate(node.left);
+    const right = evaluate(node.right);
+    if (left === null || right === null) return null;
+
+    return evaluateInfixExpression(node.operator, left, right);
   }
 
   return null;
@@ -84,4 +93,68 @@ const evalMinusPrefixExpression = (right: Object_): Object_ => {
   }
   const value = (right as Integer).value;
   return new Integer(-value);
+};
+
+const evaluateInfixExpression = (
+  operator: string,
+  left: Object_,
+  right: Object_
+): Object_ => {
+  if (left.type() === INTEGER_OBJ && right.type() === INTEGER_OBJ) {
+    return evalIntegerInfixExpression(
+      operator,
+      left as Integer,
+      right as Integer
+    );
+  } else if (left.type() === BOOLEAN_OBJ && right.type() === BOOLEAN_OBJ) {
+    return evalBooleanInfixExpression(
+      operator,
+      left as Boolean_,
+      right as Boolean_
+    );
+  } else {
+    return NULL;
+  }
+};
+
+const evalIntegerInfixExpression = (
+  operator: string,
+  left: Integer,
+  right: Integer
+): Object_ => {
+  switch (operator) {
+    case "+":
+      return new Integer(left.value + right.value);
+    case "-":
+      return new Integer(left.value - right.value);
+    case "*":
+      return new Integer(left.value * right.value);
+    case "/":
+      return new Integer(Math.floor(left.value / right.value));
+    case "<":
+      return new Boolean_(left.value < right.value);
+    case ">":
+      return new Boolean_(left.value > right.value);
+    case "==":
+      return new Boolean_(left.value === right.value);
+    case "!=":
+      return new Boolean_(left.value !== right.value);
+    default:
+      return NULL;
+  }
+};
+
+const evalBooleanInfixExpression = (
+  operator: string,
+  left: Boolean_,
+  right: Boolean_
+): Boolean_ | Null => {
+  switch (operator) {
+    case "==":
+      return new Boolean_(left.value === right.value);
+    case "!=":
+      return new Boolean_(left.value !== right.value);
+    default:
+      return NULL;
+  }
 };
